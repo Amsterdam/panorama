2b0218ff0ed79c5c6efafd7466272dd57c992cb1
Kfir Breger <kfir@cubicalwave.com>
2016-06-09 21:22:59 +0200

    Adding coords extractor to the coord search request

--- web/datapunt_open_panorama/datapunt_api/views.py
+++ web/datapunt_open_panorama/datapunt_api/views.py
@@ -1,4 +1,5 @@
 # Packages
+from django.contrib.gis.geos import Point
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import viewsets
@@ -22,12 +23,52 @@ class PanoViewSet(viewsets.ModelViewSet):
         """
         # Make sure a position is given, otherwise there is
         # nothing to work with
+        coords = self._get_request_coord(request.query_params)
         return Response([])
 
-
     def retrieve(self, request, pk=None):
-        print(pk)
         pano = get_object_or_404(Panorama, pano_id=pk)
         resp = PanoSerializer(pano)
         return Response(resp.data)
+
+    def _get_request_coord(self, query_params):
+        """
+        Retrieves the coordinates to work with. It allows for either lat/lon coord,
+        for wgs84 or 'x' and 'y' for RD. Just to be on the safe side a value check
+        a value check is done to make sure that the values are within the expected
+        range. If they seem to be RD values, an attempt is made to convert them
+        to wgs84 values
+        """
+        if 'lat' in .query_params and 'lon' in query_params:
+            lat = query_params['lat']
+            lon = query_params['lon']
+            # @TODO Doing value sanity check
+            coords = (lat, lon)
+        elif 'x' in query_params and 'y' in query_params:
+            lat = query_params['x']
+            lon = query_params['y']
+            # These should be Rd coords, convert to WGS84
+            coords = self._convert_coords(lat, lon, 28992, 4326)
+        # No good coords found
+        else:
+            return None
+        return coords
+
+    def _convert_coords(self, lat, lon, orig_srid, dest_srid):
+        """
+        Convertes a point between two coordinates
+        Parameters:
+        lat - Latitude as float
+        lon - Longitude as float
+        original_srid - The int value of the point's srid
+        dest_srid - The srid of the coordinate system to convert too
+
+        Returns
+        The new coordinates as a tuple. None if the convertion failed
+        """
+        coords = None
+        p = Point(lat, lon, srid=original_srid)
+        p.transform(dest_srid)
+        coords = p.coords
+        return coords
 
