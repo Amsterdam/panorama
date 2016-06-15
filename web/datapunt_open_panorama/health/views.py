6100866e1b85795e761c47950e21ba171ae43067
Kfir Breger <kfir@cubicalwave.com>
2016-06-15 07:45:03 +0200

    tg-250 making health check relevant

--- web/datapunt_open_panorama/health/views.py
+++ web/datapunt_open_panorama/health/views.py
@@ -4,12 +4,6 @@ import logging
 from django.conf import settings
 from django.db import connection
 from django.http import HttpResponse
-from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search
-# Project
-# from datasets.bag.models import Verblijfsobject
-# from datasets.wkpb.models import Beperking
-
 
 log = logging.getLogger(__name__)
 
@@ -26,50 +20,6 @@ def health(request):
             "Database connectivity failed",
             content_type="text/plain", status=500)
 
-    # check elasticsearch
-    try:
-        client = Elasticsearch(settings.ELASTIC_SEARCH_HOSTS)
-        assert client.info()
-    except:
-        log.exception("Elasticsearch connectivity failed")
-        return HttpResponse(
-            "Elasticsearch connectivity failed",
-            content_type="text/plain", status=500)
-
-    # check wkpd
-
     return HttpResponse(
         "Connectivity OK", content_type='text/plain', status=200)
 
-
-def check_data(request):
-    # check bag
-    # try:
-    #     assert Verblijfsobject.objects.count() > 10
-    # except:
-    #     log.exception("No BAG data found")
-    #     return HttpResponse(
-    #         "No BAG data found",
-    #         content_type="text/plain", status=500)
-    #
-    # # check wkpb
-    # try:
-    #     assert Beperking.objects.count() > 10
-    # except:
-    #     log.exception("No WKPD data found")
-    #     return HttpResponse(
-    #         "No WKPD data found",
-    #         content_type="text/plain", status=500)
-
-    # check elastic
-    try:
-        client = Elasticsearch(settings.ELASTIC_SEARCH_HOSTS)
-        assert Search().using(client).index(
-            settings.ELASTIC_INDICES['NUMMERAANDUIDING'],
-            settings.ELASTIC_INDICES['BAG']).query("match_all", size=0)
-    except:
-        log.exception("Autocomplete failed")
-        return HttpResponse(
-            "Autocomplete failed", content_type="text/plain", status=500)
-
-    return HttpResponse("Data OK", content_type='text/plain', status=200)
