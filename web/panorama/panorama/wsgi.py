d728239631e4969eaa47218e12b7ccac1878bf5d
Stephan Preeker <stephan@preeker.net>
2016-06-21 12:59:44 +0200

    rm datapunt_open

--- web/panorama/panorama/wsgi.py
+++ web/panorama/panorama/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "datapunt_open_panorama.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "panorama.settings")
 
 application = get_wsgi_application()
