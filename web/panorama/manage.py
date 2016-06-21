52e64da1e40ada04d8300fa75df51bcf9b8b6324
Stephan Preeker <stephan@preeker.net>
2016-06-21 11:47:34 +0200

    renamed everything to panorama

--- web/panorama/manage.py
+++ web/panorama/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "datapunt_open_panorama.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "panorama.settings")
 
     from django.core.management import execute_from_command_line
 
