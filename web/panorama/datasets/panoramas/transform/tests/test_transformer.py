31c6d5b7b50d366b42e490f1021bac865728d932
Marten Hoekstra <marten@dolhuis.nl>
2016-07-14 11:52:22 +0200

    Refinement: naming and formatting refined

--- web/panorama/datasets/panoramas/transform/tests/test_transformer.py
+++ web/panorama/datasets/panoramas/transform/tests/test_transformer.py
@@ -8,79 +8,79 @@ class TestTransformer(unittest.TestCase):
     def test_cylindrical2cartesion(self):
         t = PanoramaTransformer(None)
 
-        result = t.cylindrical2cartesian(4000, 2000)
+        result = t._cylindrical2cartesian(4000, 2000)
         self.assertArrayAlmostEquals([1,0,0], result)
 
-        result = t.cylindrical2cartesian(2000, 2000)
+        result = t._cylindrical2cartesian(2000, 2000)
         self.assertArrayAlmostEquals([0,-1,0], result)
 
-        result = t.cylindrical2cartesian(0, 2000)
+        result = t._cylindrical2cartesian(0, 2000)
         self.assertArrayAlmostEquals([-1,0,0], result)
 
-        result = t.cylindrical2cartesian(6000, 2000)
+        result = t._cylindrical2cartesian(6000, 2000)
         self.assertArrayAlmostEquals([0,1,0], result)
 
-        result = t.cylindrical2cartesian(4000, 1000)
+        result = t._cylindrical2cartesian(4000, 1000)
         self.assertArrayAlmostEquals([sqrt(1/2),0,sqrt(1/2)], result)
 
-        result = t.cylindrical2cartesian(4000, 3000)
+        result = t._cylindrical2cartesian(4000, 3000)
         self.assertArrayAlmostEquals([sqrt(1/2),0,-sqrt(1/2)], result)
 
-        result = t.cylindrical2cartesian(2000, 1000)
+        result = t._cylindrical2cartesian(2000, 1000)
         self.assertArrayAlmostEquals([0,-sqrt(1/2),sqrt(1/2)], result)
 
-        result = t.cylindrical2cartesian(2000, 3000)
+        result = t._cylindrical2cartesian(2000, 3000)
         self.assertArrayAlmostEquals([0,-sqrt(1/2),-sqrt(1/2)], result)
 
-        result = t.cylindrical2cartesian(0, 1000)
+        result = t._cylindrical2cartesian(0, 1000)
         self.assertArrayAlmostEquals([-sqrt(1/2),0,sqrt(1/2)], result)
 
-        result = t.cylindrical2cartesian(0, 3000)
+        result = t._cylindrical2cartesian(0, 3000)
         self.assertArrayAlmostEquals([-sqrt(1/2),0,-sqrt(1/2)], result)
 
-        result = t.cylindrical2cartesian(6000, 1000)
+        result = t._cylindrical2cartesian(6000, 1000)
         self.assertArrayAlmostEquals([0,sqrt(1/2),sqrt(1/2)], result)
 
-        result = t.cylindrical2cartesian(6000, 3000)
+        result = t._cylindrical2cartesian(6000, 3000)
         self.assertArrayAlmostEquals([0,sqrt(1/2),-sqrt(1/2)], result)
 
     def test_cartesian2cylindrical(self):
         t = PanoramaTransformer(None)
 
-        result = t.cartesian2cylindrical(1,0,0)
+        result = t._cartesian2cylindrical(1, 0, 0)
         self.assertArrayAlmostEquals([4000, 2000], result)
 
-        result = t.cartesian2cylindrical(0,-1,0)
+        result = t._cartesian2cylindrical(0, -1, 0)
         self.assertArrayAlmostEquals([2000, 2000], result)
 
-        result = t.cartesian2cylindrical(-1,0,0)
+        result = t._cartesian2cylindrical(-1, 0, 0)
         self.assertArrayAlmostEquals([0, 2000], result)
 
-        result = t.cartesian2cylindrical(0,1,0)
+        result = t._cartesian2cylindrical(0, 1, 0)
         self.assertArrayAlmostEquals([6000, 2000], result)
 
-        result = t.cartesian2cylindrical(sqrt(1/2),0,sqrt(1/2))
+        result = t._cartesian2cylindrical(sqrt(1 / 2), 0, sqrt(1 / 2))
         self.assertArrayAlmostEquals([4000, 1000], result)
 
-        result = t.cartesian2cylindrical(sqrt(1/2),0,-sqrt(1/2))
+        result = t._cartesian2cylindrical(sqrt(1 / 2), 0, -sqrt(1 / 2))
         self.assertArrayAlmostEquals([4000, 3000], result)
 
-        result = t.cartesian2cylindrical(0,-sqrt(1/2),sqrt(1/2))
+        result = t._cartesian2cylindrical(0, -sqrt(1 / 2), sqrt(1 / 2))
         self.assertArrayAlmostEquals([2000, 1000], result)
 
-        result = t.cartesian2cylindrical(0,-sqrt(1/2),-sqrt(1/2))
+        result = t._cartesian2cylindrical(0, -sqrt(1 / 2), -sqrt(1 / 2))
         self.assertArrayAlmostEquals([2000, 3000], result)
 
-        result = t.cartesian2cylindrical(-sqrt(1/2),0,sqrt(1/2))
+        result = t._cartesian2cylindrical(-sqrt(1 / 2), 0, sqrt(1 / 2))
         self.assertArrayAlmostEquals([0, 1000], result)
 
-        result = t.cartesian2cylindrical(-sqrt(1/2),0,-sqrt(1/2))
+        result = t._cartesian2cylindrical(-sqrt(1 / 2), 0, -sqrt(1 / 2))
         self.assertArrayAlmostEquals([0, 3000], result)
 
-        result = t.cartesian2cylindrical(0,sqrt(1/2),sqrt(1/2))
+        result = t._cartesian2cylindrical(0, sqrt(1 / 2), sqrt(1 / 2))
         self.assertArrayAlmostEquals([6000, 1000], result)
 
-        result = t.cartesian2cylindrical(0,sqrt(1/2),-sqrt(1/2))
+        result = t._cartesian2cylindrical(0, sqrt(1 / 2), -sqrt(1 / 2))
         self.assertArrayAlmostEquals([6000, 3000], result)
 
     def assertArrayAlmostEquals(self, expected, actual):
--- web/panorama/datasets/panoramas/transform/transformer.py
+++ web/panorama/datasets/panoramas/transform/transformer.py
@@ -2,13 +2,14 @@ from numpy import array, radians, float64, pi, arctan2, arccos, cos, sin, arange
 from scipy import misc
 
 
-#   general property of pano set source image
-SOURCE_WIDTH = 8000             # pixels
+# specific property of our pano set source images
+SOURCE_WIDTH = 8000     # pixels
 
-# properties of equirectangular projections
-PANO_ANGLE = 360              # degrees
-PANO_HORIZON = 0.5            # fraction of image that is below horizon
-PANO_ASPECT = 2               # width over height
+# general properties of equirectangular projections
+NORTH_0_DEGREES = 0     # default/base heading
+PANO_FOV = 360          # field of view in degrees
+PANO_HORIZON = 0.5      # fraction of image that is below horizon
+PANO_ASPECT = 2         # width over height
 PANO_HEIGHT = SOURCE_WIDTH / PANO_ASPECT
 
 
@@ -18,35 +19,35 @@ class PanoramaTransformer:
         self.panorama = panorama
 
     def get_translated_image(self,
-                             target_width=4000,
-                             target_angle=360,
-                             target_horizon=0.5,
-                             target_heading=0,
-                             target_aspect=2):
+                             target_width=SOURCE_WIDTH,
+                             target_fov=PANO_FOV,
+                             target_horizon=PANO_HORIZON,
+                             target_heading=NORTH_0_DEGREES,
+                             target_aspect=PANO_ASPECT):
 
         # create the target pixel set expressed as coordinates of a normalized equirectangular view of given source-size
-        x, y = self.create_sample_set(target_angle, target_aspect, target_heading, target_horizon, target_width)
+        x, y = self._create_sample_set(target_fov, target_aspect, target_heading, target_horizon, target_width)
 
         # transform image coordinates in equirectangular projection to cartesian vectors with r=1
-        x1, y1, z1 = self.cylindrical2cartesian(x, y)
+        x1, y1, z1 = self._cylindrical2cartesian(x, y)
 
         # rotate vectors according to rotation-matrix for pitch and roll
-        m = self.get_rotation_matrix(self.panorama.pitch, self.panorama.roll)
+        m = self._get_rotation_matrix(self.panorama.pitch, self.panorama.roll)
 
         x2 = m[0][0] * x1 + m[0][1] * y1 + m[0][2] * z1
         y2 = m[1][0] * x1 + m[1][1] * y1 + m[1][2] * z1
         z2 = m[2][0] * x1 + m[2][1] * y1 + m[2][2] * z1
 
         # transform cartesion vectors back to image coordinates in a equirectangular projection
-        x3, y3 = self.cartesian2cylindrical(x2, y2, z2)
+        x3, y3 = self._cartesian2cylindrical(x2, y2, z2)
 
         # return grid of output pixels from source image based on warped coordinates
         return misc.imread(self.panorama.get_full_path())[y3[:, :], x3[:, :]]
 
-    def create_sample_set(self, target_angle, target_aspect, target_heading, target_horizon, target_width):
+    def _create_sample_set(self, target_angle, target_aspect, target_heading, target_horizon, target_width):
         target_center = SOURCE_WIDTH / 2 - (self.panorama.heading - target_heading) * SOURCE_WIDTH / 360
 
-        chunk_width = SOURCE_WIDTH * target_angle / PANO_ANGLE
+        chunk_width = SOURCE_WIDTH * target_angle / PANO_FOV
         chunk_height = chunk_width / target_aspect
         chunk_above_horizon = chunk_height * (1 - target_horizon)
 
@@ -60,7 +61,7 @@ class PanoramaTransformer:
         return meshgrid(arange(left_top_x, right_bottom_x, steps),
                            arange(left_top_y, right_bottom_y, steps))
 
-    def cylindrical2cartesian(self, x, y):
+    def _cylindrical2cartesian(self, x, y):
         phi = (x - PANO_HEIGHT) * pi / PANO_HEIGHT
         theta = (y * pi) / PANO_HEIGHT
 
@@ -70,22 +71,30 @@ class PanoramaTransformer:
 
         return x1, y1, z1
 
-    def get_rotation_matrix(self, pitch, roll):
+    def _get_rotation_matrix(self, pitch, roll):
         pitch_rad = radians(pitch)
         roll_rad = radians(roll)
 
-        pitch_matrix = array([[cos(pitch_rad), 0, sin(pitch_rad)],
-                                 [0, 1, 0],
-                                 [-1 * sin(pitch_rad), 0, cos(pitch_rad)]],
-                                dtype=float64)
-        roll_matrix = array([[1, 0, 0],
-                                [0, cos(roll_rad), -1 * sin(roll_rad)],
-                                [0, sin(roll_rad), cos(roll_rad)]],
-                               dtype=float64)
+        pitch_matrix = array(
+            [
+                [cos(pitch_rad), 0, sin(pitch_rad)],
+                [0, 1, 0],
+                [-1 * sin(pitch_rad), 0, cos(pitch_rad)]
+            ],
+            dtype=float64
+        )
+        roll_matrix = array(
+            [
+                [1, 0, 0],
+                [0, cos(roll_rad), -1 * sin(roll_rad)],
+                [0, sin(roll_rad), cos(roll_rad)]
+            ],
+            dtype=float64
+        )
 
         return pitch_matrix.dot(roll_matrix)
 
-    def cartesian2cylindrical(self, x, y, z):
+    def _cartesian2cylindrical(self, x, y, z):
         theta = arccos(z)
         phi = arctan2(y, x)
 
