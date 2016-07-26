6a7df43e12d959c14886ac493fb6796992a4668c
Marten Hoekstra <marten@dolhuis.nl>
2016-07-26 13:41:59 +0200

    TG-775 Tests voor taken die JIRA aanroept

--- web/panorama/datasets/tasks/models.py
+++ web/panorama/datasets/tasks/models.py
@@ -5,4 +5,4 @@ class RenderTask(models.Model):
     pano_id = models.CharField(max_length=37, primary_key=True)
 
     def __str__(self):
-        return '<RenderTask %d>' % self.pano_id
+        return '<RenderTask %s>' % self.pano_id
