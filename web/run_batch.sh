926a086fd73de9a4c40ea264cf960f6b383166b5
Marten Hoekstra <marten@dolhuis.nl>
2016-10-17 13:45:38 +0200

    Refinment docs en fixt tests

--- web/run_batch.sh
+++ web/run_batch.sh
@@ -4,13 +4,13 @@ for f in parameterfile.par
 do
   echo "Processing $f"
   while read p; do
-    (docker -H :4000 run -d -m 3600m master.swarm.datapunt.amsterdam.nl:5000/panojob $p)
+    (docker -H :4000 run -d -m 4G master.swarm.datapunt.amsterdam.nl:5000/panojob $p)
     until [ $? == 0 ]
     do
 	  sleep 1
       echo "cleaning exited containers"
 	  for container_id in $(docker -H :4000 ps -a --filter status=exited -q);do docker -H :4000 rm $container_id;done
-	  echo "retry $p"
+	  echo "retry parameters $p"
 	  (docker -H :4000 run -d -m 4G master.swarm.datapunt.amsterdam.nl:5000/panojob $p)
     done
   done <$f
