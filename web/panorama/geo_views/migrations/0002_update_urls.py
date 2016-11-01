ed79d8599a462e1e7081491e3ee61f3a10385ce3
Marten Hoekstra <marten@dolhuis.nl>
2016-11-01 17:19:38 +0100

    TG-354 Update de urls in de geoview

---
+++ web/panorama/geo_views/migrations/0002_update_urls.py
@@ -0,0 +1,41 @@
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+from django.conf import settings
+
+from geo_views import migrate
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('geo_views', '0001_initial'),
+    ]
+
+    operations = [
+        migrate.ManageView(
+            view_name="geo_panoramas_panoramafotopunt",
+            sql="""
+SELECT
+    pp.id,
+    pp.pano_id as display,
+    pp.roll,
+    pp.pitch,
+    pp.heading,
+    pp.timestamp,
+    pp.geolocation AS geometrie,
+    'https://atlas.amsterdam.nl/panorama/' || pp.path || trim(trailing '.jpg' from pp.filename)
+    || '/equirectangular/panorama_8000.jpg' AS url,
+    site.domain || 'panorama/opnamelocatie/' || pp.pano_id || '/' AS uri
+FROM
+    panoramas_panorama pp,
+    django_site site
+WHERE
+    site.name = 'API Domain'
+AND
+    pp.geolocation IS NOT NULL
+"""
+        ),
+
+    ]
