d68a8a0369935d9b77c9f14a4e9b5833308bcd61
Marten Hoekstra <marten@dolhuis.nl>
2016-11-29 15:22:32 +0100

    TG-105: proefdraaien meer in balans

--- web/panorama/datapunt/management/commands/schedulers/schedule_detection.py
+++ web/panorama/datapunt/management/commands/schedulers/schedule_detection.py
@@ -32,7 +32,7 @@ class DetectionScheduler(Scheduler, PanoramaTableAware):
         messages = []
         max_id = Panorama.objects.all().order_by("-id")[0].id
         rand_int = randrange(max_id+1)
-        for panorama in Panorama.rendered.all()[rand_int:rand_int+500]:
+        for panorama in Panorama.rendered.all()[rand_int:rand_int+50]:
             log.info("Sending detection tasks for: {}".format(panorama.pano_id))
             messages.append({'pano_id': panorama.pano_id,
                              'panorama_url': panorama.equirectangular_img_urls['full']
