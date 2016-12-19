5e7db970795b73b6028e3381b8072dbb89b93282
Marten Hoekstra <marten@dolhuis.nl>
2016-12-19 15:05:08 +0100

    TG-105 Backend: import job herkent nu ook gedetecteerde foto's

--- web/panorama/datasets/panoramas/models.py
+++ web/panorama/datasets/panoramas/models.py
@@ -21,7 +21,8 @@ PREVIEW_IMAGE = 'preview.jpg'
 
 
 class Panorama(StatusModel):
-    STATUS = Choices('to_be_rendered', 'rendering', 'rendered', 'blurring', 'detecting2', 'blurred')
+    STATUS = Choices('to_be_rendered', 'rendering', 'rendered', 'detecting_lp', 'detected_lp', 'detecting1',
+                     'detected_1', 'detecting2', 'detected_2', 'blurring', 'blurred')
 
     id = models.AutoField(primary_key=True)
     pano_id = models.CharField(max_length=37, unique=True)
