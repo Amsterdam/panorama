2a060826d9103de4e1979e36cdad83d64db135e8
Marten Hoekstra <marten@dolhuis.nl>
2017-01-10 15:09:56 +0100

    TG-105 Backend: meer werk op swarm en grovere blurring, er bleef teveel zichtbaar

--- web/panorama/datapunt/management/commands/schedulers/schedule_rendering.py
+++ web/panorama/datapunt/management/commands/schedulers/schedule_rendering.py
@@ -15,11 +15,11 @@ class RenderScheduler(Scheduler, PanoramaTableAware):
         with self.panorama_table_present():
             while True:
                 self.queue_result()
-                time.sleep(240)
+                time.sleep(15)
 
     def get_messages(self):
         messages = []
-        for panorama in Panorama.to_be_rendered.all()[:5]:
+        for panorama in Panorama.to_be_rendered.all()[:50]:
             log.info("Sending render task: {}".format(panorama.pano_id))
 
             messages.append({'pano_id': panorama.pano_id,
