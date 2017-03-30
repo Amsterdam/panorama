99c3f725e96cabc1c994fcc63fb1b34bce2c95e7
Marten Hoekstra <marten@dolhuis.nl>
2017-03-30 17:33:42 +0200

    tg-920: fixt workers op swarm

--- web/panorama/panorama/tasks/workers/__init__.py
+++ web/panorama/panorama/tasks/workers/__init__.py
@@ -2,7 +2,7 @@ from . import detect_faces, detect_lps, detect_faces_dlib, blur_regions, detect_
 
 workers = [
     blur_regions.BlurRegions,
-    detect_faces_google.DetectFacesDlib,
+    detect_faces_google.DetectFacesGoogle,
     detect_faces_dlib.DetectFacesDlib,
     detect_lps.DetectLicensePlates,
     detect_faces.DetectFaces,
