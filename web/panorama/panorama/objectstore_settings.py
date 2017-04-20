a54cc2760eeb3e3dba84fe58de6d544a0fe87a1c
Marten Hoekstra <marten@dolhuis.nl>
2017-04-20 17:20:06 +0200

    Start import 2017

--- web/panorama/panorama/objectstore_settings.py
+++ web/panorama/panorama/objectstore_settings.py
@@ -14,12 +14,7 @@ PANORAMA_TENANT_NAME = 'BGE000081 Panorama'
 PANORAMA_TENANT_ID = '3206eec333a04cc980799f75a593505a'
 
 #   wait for new years to be uploaded:
-#   when enabling this also views should be changed
-#   in such a way that the only show Panoramas with status done
-#   See taiga #599 API methods: alleen teurggeven geblurde panorama's (ook bij adjecencies)
-#   replacing Panorama.objects. with Panorama.done. should do a lot. Maybe extra query shizzle is neede.
-# PANORAMA_CONTAINERS = ['2016', '2017']
-PANORAMA_CONTAINERS = ['2016']
+PANORAMA_CONTAINERS = ['2016', '2017']
 
 DATAPUNT_CONTAINER = 'panorama'
 
