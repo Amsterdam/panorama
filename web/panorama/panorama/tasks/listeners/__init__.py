4c1cfd989520b698a53850233e0d89642c9b23e1
Marten Hoekstra <marten@dolhuis.nl>
2017-04-25 11:59:41 +0200

    Moved rendering to the swarm

--- web/panorama/panorama/tasks/listeners/__init__.py
+++ web/panorama/panorama/tasks/listeners/__init__.py
@@ -1,6 +1,8 @@
-from . import face_detected, licenseplate_detected, dlib_face_detected, blurring_done, google_face_detected
+from . import face_detected, licenseplate_detected, dlib_face_detected, blurring_done, \
+    google_face_detected, panorama_rendered
 
 listeners = [
+    panorama_rendered.RenderingDone,
     blurring_done.BlurDone,
     dlib_face_detected.FaceDone,
     face_detected.FaceDone,
--- web/panorama/panorama/tasks/workers/__init__.py
+++ web/panorama/panorama/tasks/workers/__init__.py
@@ -1,6 +1,7 @@
-from . import detect_faces, detect_lps, detect_faces_dlib, blur_regions, detect_faces_google
+from . import detect_faces, detect_lps, detect_faces_dlib, blur_regions, detect_faces_google, render_pano
 
 workers = [
+    render_pano.RenderPano,
     blur_regions.BlurRegions,
     detect_faces_google.DetectFacesGoogle,
     detect_faces_dlib.DetectFacesDlib,
