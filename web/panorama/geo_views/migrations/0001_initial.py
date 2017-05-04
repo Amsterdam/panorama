a171280ee8856117d027477af4a348f62e2cb31d
Thijs Creemers <thijs.creemers@gmail.com>
2017-05-04 10:49:36 +0200

    remove django.contrib.sites references from migration

--- web/panorama/geo_views/migrations/0001_initial.py
+++ web/panorama/geo_views/migrations/0001_initial.py
@@ -3,35 +3,15 @@
 from __future__ import unicode_literals
 
 from django.db import migrations
-
-from django.conf import settings
-
 from geo_views import migrate
 
 
-def create_site(apps, *args, **kwargs):
-    Site = apps.get_model('sites', 'Site')
-    Site.objects.create(
-        domain=settings.DATAPUNT_API_URL,
-        name='API Domain'
-    )
-
-
-def delete_site(apps, *args, **kwargs):
-    Site = apps.get_model('sites', 'Site')
-    Site.objects.filter(name='API Domain').delete()
-
-
 class Migration(migrations.Migration):
-
     dependencies = [
-            ('sites', '0001_initial'),
-            ('panoramas', '0001_initial')
+        ('panoramas', '0001_initial')
     ]
 
     operations = [
-        migrations.RunPython(code=create_site, reverse_code=delete_site),
-
         migrate.ManageView(
             view_name="geo_panoramas_panoramafotopunt",
             sql="""
@@ -44,16 +24,11 @@ SELECT
     pp.timestamp,
     pp.geolocation AS geometrie,
     'https://acc.atlas.amsterdam.nl/panorama' || pp.path || '/' ||
-    pp.filename AS url,
-    site.domain || 'panoramas/opnamelocatie/' || pp.pano_id || '/' AS uri
+    pp.filename AS url, 'panoramas/opnamelocatie/' || pp.pano_id || '/' AS uri
 FROM
-    panoramas_panorama pp,
-    django_site site
-WHERE
-    site.name = 'API Domain'
+    panoramas_panorama pp
 AND
     pp.geolocation IS NOT NULL
 """
         ),
-
     ]
