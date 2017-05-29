47e2581b4999da50d4e85472b408351baa0d7a2e
Marten Hoekstra <marten@dolhuis.nl>
2017-05-29 14:30:55 +0200

    Single threaded import on swarm

--- web/panorama/datasets/panoramas/derived_models.py
+++ web/panorama/datasets/panoramas/derived_models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from . models import AbstractPanorama, AbstractAdjacency
+from django.conf import settings
 
 
 class RecentPanorama(AbstractPanorama):
@@ -16,3 +17,22 @@ class RecentAdjacency(AbstractAdjacency):
     class Meta(AbstractAdjacency.Meta):
         abstract = False
         db_table = "panoramas_adjacencies_recent_all"
+
+
+for year in settings.PREPARED_YEARS:
+    exec(f"""
+class RecentPanorama{year}(AbstractPanorama):
+    class Meta(AbstractPanorama.Meta):
+        abstract = False
+        managed = False
+        db_table = "panoramas_recent_{year}"
+
+
+class RecentAdjacency{year}(AbstractAdjacency):
+    from_pano = models.ForeignKey(RecentPanorama{year}, related_name='to_adjacency')
+    to_pano = models.ForeignKey(RecentPanorama{year}, related_name='from_adjacency')
+
+    class Meta(AbstractAdjacency.Meta):
+        abstract = False
+        db_table = "panoramas_adjacencies_recent_{year}"
+    """)
