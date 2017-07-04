5c2af59e2692dea1a41cd39dbabf98239176d03e
Marten Hoekstra <marten@dolhuis.nl>
2017-07-04 16:38:06 +0200

    TG-1050 backend: list view gebruikt ook de serializer_detail_class

--- web/panorama/panorama/derived_views.py
+++ web/panorama/panorama/derived_views.py
@@ -5,23 +5,17 @@ from . views import PanoramaViewSet
 
 class RecentPanoramaViewSet(PanoramaViewSet):
     def list(self, request, *args, **kwargs):
-        self._set_queryset_and_serializer(request)
+        self._set_queryset_and_serializers(request)
         return super().list(request, *args, **kwargs)
 
     def retrieve(self, request, *args, **kwargs):
-         self._set_queryset_and_detail_serializer(request)
-         return super().retrieve(request, *args, **kwargs)
+        self._set_queryset_and_serializers(request)
+        return super().retrieve(request, *args, **kwargs)
 
-    def _set_queryset_and_serializer(self, request):
-        self.recent_pano_model_class = models.getRecentPanoModel(request.path)
-        self.queryset = self.recent_pano_model_class.done.all()
+    def _set_queryset_and_serializers(self, request):
+        recent_pano_model_class = models.getRecentPanoModel(request.path)
+        self.queryset = recent_pano_model_class.done.all()
         self.serializer_class = serializers.getRecentPanoSerializer(
-            self.recent_pano_model_class, request.path
-        )
-
-    def _set_queryset_and_detail_serializer(self, request):
-        self._set_queryset_and_serializer(request)
+            recent_pano_model_class, request.path)
         self.serializer_detail_class = serializers.getFilteredRecentPanoSerializer(
-            self.recent_pano_model_class, request.path
-        )
-
+            recent_pano_model_class, request.path)
