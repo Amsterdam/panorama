f574cf8e4ab6f29b16eef97b318139a5d8f790b2
Marten Hoekstra <marten@dolhuis.nl>
2017-07-10 14:25:44 +0200

    tg-606: lokale docker-compose service stack toegevoegd, begin met save-db.sh gemaakt (incompleet) en readme.md aangepast

--- .swarm/database/z_import_panorama.sh
+++ .swarm/database/z_import_panorama.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# use the packaged updatescript in the background to not mess with entrypoint script
-/bin/update-db.sh panorama &
+# use the packaged single-thread and single-transaction update script
+#   in the background as to not to mess with entrypoint script
+
+/bin/update-single.sh panorama &
 
 echo STARTED UPDATE SCRIPT
