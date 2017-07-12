4ae4a447b600e9a77c8e9a7cdf4e31d9eb5953d9
Marten Hoekstra <marten@dolhuis.nl>
2017-07-12 15:55:57 +0200

    tg-606: refinement in script

--- .swarm/save-db.sh
+++ .swarm/save-db.sh
@@ -7,8 +7,6 @@ fi
 
 _db_docker=`docker ps -q -f "name=panoswarm_database"`
 
-echo EXPORTING DATA FROM CONTAINTER $_db_docker
-
 docker exec -it $_db_docker psql -U panorama -c 'drop table if exists region_export'
 docker exec -it $_db_docker psql -U panorama -c 'create table region_export as select region_type, left_top_x, left_top_y, right_top_x, right_top_y, left_bottom_x, left_bottom_y, right_bottom_x, right_bottom_y, detected_by, pano_id from panoramas_region'
 docker exec -it $_db_docker pg_dump -Fc -t region_export* -d panorama -U panorama -f /tmp/exported_regions.sql
