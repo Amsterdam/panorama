b18a71ca5b6732da3a6362b046b33f3abe05446a
Marten Hoekstra <marten@dolhuis.nl>
2017-07-12 11:00:57 +0200

    tg-606: injectie secrets in ansible

--- .swarm/database/z_import_panorama.sh
+++ .swarm/database/z_import_panorama.sh
@@ -3,6 +3,6 @@
 # use the packaged single-thread and single-transaction update script
 #   in the background as to not to mess with entrypoint script
 
-#/bin/import-single.sh panorama &
+/bin/import-single.sh panorama &
 
 echo STARTED UPDATE SCRIPT
