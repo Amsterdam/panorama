32114acfd22a6588a95df67343e69ba98c32db8b
Marten Hoekstra <marten@dolhuis.nl>
2017-09-11 15:54:48 +0200

    tg-1201: zorg dat thumbnails uit de juiste set komen.

--- web/panorama/panorama/derived_views.py
+++ web/panorama/panorama/derived_views.py
@@ -5,17 +5,17 @@ from . views import PanoramaViewSet
 
 class RecentPanoramaViewSet(PanoramaViewSet):
     def list(self, request, *args, **kwargs):
-        self._set_queryset_and_serializers(request)
+        self._set_queryset_and_serializers(request.path)
         return super().list(request, *args, **kwargs)
 
     def retrieve(self, request, *args, **kwargs):
-        self._set_queryset_and_serializers(request)
+        self._set_queryset_and_serializers(request.path)
         return super().retrieve(request, *args, **kwargs)
 
-    def _set_queryset_and_serializers(self, request):
-        recent_pano_model_class = models.getRecentPanoModel(request.path)
+    def _set_queryset_and_serializers(self, path):
+        recent_pano_model_class = models.getRecentPanoModel(path)
         self.queryset = recent_pano_model_class.done.all()
         self.serializer_class = serializers.getRecentPanoSerializer(
-            recent_pano_model_class, request.path)
+            recent_pano_model_class, path)
         self.serializer_detail_class = serializers.getFilteredRecentPanoSerializer(
-            recent_pano_model_class, request.path)
+            recent_pano_model_class, path)
