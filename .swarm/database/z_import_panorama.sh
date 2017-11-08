88d7725bf76f73696ba73f958c9d395713b78160
Marten Hoekstra <marten@dolhuis.nl>
2017-11-08 10:33:32 +0100

    Fixes swarm database script in combination with new db-download with Datapunt DB Docker

--- .swarm/database/z_import_panorama.sh
+++ .swarm/database/z_import_panorama.sh
@@ -3,6 +3,6 @@
 # use the packaged single-thread and single-transaction update script
 #   in the background as to not to mess with entrypoint script
 
-/bin/import-single.sh panorama &
+/bin/import-single.sh panorama root &
 
 echo STARTED UPDATE SCRIPT
