acf8a4bb97dc39f7647d72479811fb17495af678
Marten Hoekstra <marten@dolhuis.nl>
2017-11-28 21:26:03 +0100

    Minder belasting op de swarm database

--- .swarm/database/z_import_panorama.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# use the packaged update scripts
-#   in the background as to not to mess with the other entrypoint scripts
-
-/bin/dl_and_import_for_swarm.sh &
-
-echo STARTED DOWNLOAD AND IMPORT SCRIPT
