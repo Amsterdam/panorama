9e69ad6b0a57dffa09bb1ef7849e634bab812662
Marten Hoekstra <marten@dolhuis.nl>
2017-11-29 11:32:21 +0100

    Remove interactiviteit om de docker error 'this is not a TTY' te voorkomen

--- .swarm/dl_and_load_data.sh
+++ .swarm/dl_and_load_data.sh
@@ -2,6 +2,6 @@
 
 _db_docker=`docker ps -q -f "name=panoswarm_database"`
 
-docker exec -it $_db_docker /bin/update-table.sh panorama panoramas_region public panorama
-docker exec -it $_db_docker /bin/update-table.sh panorama panoramas_panorama public panorama
+docker exec $_db_docker /bin/update-table.sh panorama panoramas_region public panorama
+docker exec $_db_docker /bin/update-table.sh panorama panoramas_panorama public panorama
 
