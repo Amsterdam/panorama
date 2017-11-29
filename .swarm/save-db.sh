4620b5e5a6c51cdd6fa5e585c164cd03da08ca14
Marten Hoekstra <marten@dolhuis.nl>
2017-11-29 11:34:00 +0100

    Remove interactiviteit om de docker error 'this is not a TTY' te voorkomen

--- .swarm/save-db.sh
+++ .swarm/save-db.sh
@@ -7,8 +7,8 @@ fi
 
 _db_docker=`docker ps -q -f "name=panoswarm_database"`
 
-docker exec -it $_db_docker psql -U panorama -c 'drop table if exists region_export'
-docker exec -it $_db_docker psql -U panorama -c 'create table region_export as select region_type, left_top_x, left_top_y, right_top_x, right_top_y, left_bottom_x, left_bottom_y, right_bottom_x, right_bottom_y, detected_by, pano_id from panoramas_region'
-docker exec -it $_db_docker pg_dump -Fc -t region_export* -d panorama -U panorama -f /tmp/exported_regions.dmp
+docker exec $_db_docker psql -U panorama -c 'drop table if exists region_export'
+docker exec $_db_docker psql -U panorama -c 'create table region_export as select region_type, left_top_x, left_top_y, right_top_x, right_top_y, left_bottom_x, left_bottom_y, right_bottom_x, right_bottom_y, detected_by, pano_id from panoramas_region'
+docker exec $_db_docker pg_dump -Fc -t region_export* -d panorama -U panorama -f /tmp/exported_regions.dmp
 docker cp $_db_docker:/tmp/exported_regions.dmp /tmp/exported_regions.dmp
 cp /tmp/exported_regions.dmp /home/archive/exported_regions.dmp
