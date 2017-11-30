57afac133966ad638c5fb8d6d4751e940d99f513
Marten Hoekstra <marten@dolhuis.nl>
2017-11-30 12:27:58 +0100

    Fixt swarm startup

--- .swarm/dl_and_load_data.sh
+++ .swarm/dl_and_load_data.sh
@@ -2,6 +2,8 @@
 
 _db_docker=`docker ps -q -f "name=panoswarm_database"`
 
+docker exec $_db_docker /bin/download-db.sh panorama
 docker exec $_db_docker /bin/update-table.sh panorama panoramas_region public panorama
+docker exec $_db_docker psql -U panorama -c 'create sequence panoramas_region_id_seq'
+docker exec $_db_docker psql -U panorama -c "alter table panoramas_region alter id set default nextval('panoramas_region_id_seq')"
 docker exec $_db_docker /bin/update-table.sh panorama panoramas_panorama public panorama
-
