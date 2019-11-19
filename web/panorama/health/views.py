30ec83a755bed868042177fe2fa9539e24b5940f
Marten Hoekstra <marten@dolhuis.nl>
2019-11-19 14:00:55 +0100

    No DB check in Health check

--- web/panorama/health/views.py
+++ web/panorama/health/views.py
@@ -29,10 +29,10 @@ def health(request):
             "Debug mode not allowed in production",
             content_type="text/plain", status=500)
 
-    if Panoramas.objects.all().count() < 500000:
-        return HttpResponse(
-            "Too few Panoramas in the database",
-            content_type="text/plain", status=500)
+    # if Panoramas.objects.all().count() < 500000:
+    #     return HttpResponse(
+    #         "Too few Panoramas in the database",
+    #         content_type="text/plain", status=500)
 
     return HttpResponse(
         "Health OK", content_type='text/plain', status=200)
