4f0001095622c3e394054a8eaba4fc8187fc2b9d
LB <78799392+lbam@users.noreply.github.com>
2023-06-14 15:21:54 +0200

    Cleanup of math code

--- web/panorama/panorama/transform/utils_math_array.py
+++ web/panorama/panorama/transform/utils_math_array.py
@@ -1,56 +1,30 @@
-from numpy import array, sqrt, square, radians, float64, pi, arctan2, arccos, cos, sin, mod
+import numpy as np
+from scipy.spatial.transform import Rotation
 
 
 def get_rotation_matrix(yaw, pitch, roll):
-    rad_pitch = radians(pitch)
-    rad_roll = radians(roll)
-    rad_yaw = radians(-yaw)
-
-    rot_x_roll = array(
-        [
-            [1, 0, 0],
-            [0, cos(rad_roll), -sin(rad_roll)],
-            [0, sin(rad_roll), cos(rad_roll)]
-        ],
-        dtype=float64
-    )
-    rot_y_pitch = array(
-        [
-            [cos(rad_pitch), 0, sin(rad_pitch)],
-            [0, 1, 0],
-            [-sin(rad_pitch), 0, cos(rad_pitch)]
-        ],
-        dtype=float64
-    )
-    rot_z_yaw = array(
-        [
-            [cos(rad_yaw), -sin(rad_yaw), 0],
-            [sin(rad_yaw), cos(rad_yaw), 0],
-            [0, 0, 1]
-        ],
-        dtype=float64
-    )
-
-    return rot_x_roll.dot(rot_y_pitch).dot(rot_z_yaw)
+    r = Rotation.from_euler("zyx", [-yaw, pitch, roll], degrees=True)
+    return r.as_matrix()
 
 
 def cylindrical2cartesian(coordinates, source_width, source_height):
     middle = source_width / 2
 
-    x = coordinates[0]
-    y = coordinates[1]
+    x, y = coordinates
 
-    phi = (x - middle) * pi / middle
-    theta = (y * pi) / source_height
+    phi = (x - middle) * np.pi / middle
+    theta = (y * np.pi) / source_height
 
-    x1 = sin(theta)*cos(phi)
-    y1 = sin(theta)*sin(phi)
-    z1 = cos(theta)
+    sin_theta = np.sin(theta)
+    x1 = sin_theta * np.cos(phi)
+    y1 = sin_theta * np.sin(phi)
+    z1 = np.cos(theta)
 
     return x1, y1, z1
 
 
 def rotate_cartesian_vectors(vector, matrix):
+    # TODO rewrite as numpy.dot.
     x = vector[0]
     y = vector[1]
     z = vector[2]
@@ -72,11 +46,11 @@ def cartesian2cylindrical(vector, source_width, source_height, r_is_1=True):
     y = vector[1]
     z = vector[2]
 
-    r = 1 if r_is_1 else sqrt(square(x) + square(y) + square(z))
-    theta = arccos(z/r)
-    phi = arctan2(y, x)
+    r = 1 if r_is_1 else np.linalg.norm([x, y, z])
+    theta = np.arccos(z / r)
+    phi = np.arctan2(y, x)
 
-    x1 = mod(middle + middle * phi / pi, source_width-1)
-    y1 = source_height * theta / pi
+    x1 = np.mod(middle + middle * phi / np.pi, source_width - 1)
+    y1 = source_height * theta / np.pi
 
     return x1, y1
