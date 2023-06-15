ef891542e020eb438e2dd4067487a6fbcbe8e724
LB <78799392+lbam@users.noreply.github.com>
2023-06-15 08:51:47 +0200

    Remove unused cubic transformation code

--- web/panorama/panorama/transform/cubic.py
+++ web/panorama/panorama/transform/cubic.py
@@ -2,17 +2,23 @@ from numpy import arange, meshgrid
 
 from . import utils_img_file as Img
 from . import utils_math_array as Math
-from . import utils_math_cubic as Cube
 from . transformer import SOURCE_WIDTH, PANO_HEIGHT, BasePanoramaTransformer
+
+
+CUBE_FRONT, CUBE_BACK, CUBE_LEFT, CUBE_RIGHT, CUBE_UP, CUBE_DOWN = 'f', 'b', 'l', 'r', 'u', 'd'
+#   preserve order - the preview.jpg in utils_img_file_set depends on it:
+CUBE_SIDES = [CUBE_BACK, CUBE_DOWN, CUBE_FRONT, CUBE_LEFT, CUBE_RIGHT, CUBE_UP]
+
+MAX_CUBIC_WIDTH = 2048  # width of cubic edges
 
 
 class CubicTransformer(BasePanoramaTransformer):
 
-    def get_projection(self, target_width=Cube.MAX_CUBIC_WIDTH):
+    def get_projection(self, target_width=MAX_CUBIC_WIDTH):
         cube_projections = {}
 
         # project to sides
-        for direction in Cube.CUBE_SIDES:
+        for direction in CUBE_SIDES:
             # get target pixel set of cube side (cartesian, where r =/= 1, but depends on cube form)
             x, y, z = self._get_cube_side(direction, target_width)
 
@@ -30,9 +36,9 @@ class CubicTransformer(BasePanoramaTransformer):
 
         return cube_projections
 
-    def get_normalized_projection(self, target_width=Cube.MAX_CUBIC_WIDTH):
+    def get_normalized_projection(self, target_width=MAX_CUBIC_WIDTH):
         cube_projections = {}
-        for direction in Cube.CUBE_SIDES:
+        for direction in CUBE_SIDES:
             x2, y2 = Math.cartesian2cylindrical(self._get_cube_side(direction, target_width),
                                                 source_width=SOURCE_WIDTH,
                                                 source_height=PANO_HEIGHT,
@@ -47,27 +53,27 @@ class CubicTransformer(BasePanoramaTransformer):
         x, y, z = (0, 0, 0)
         half_width = width / 2
 
-        if side == Cube.CUBE_FRONT:
+        if side == CUBE_FRONT:
             x = half_width
             y, z = meshgrid(arange(-half_width, half_width, 1),
                             arange(half_width, -half_width, -1))
-        elif side == Cube.CUBE_BACK:
+        elif side == CUBE_BACK:
             x = -half_width
             y, z = meshgrid(arange(half_width, -half_width, -1),
                             arange(half_width, -half_width, -1))
-        elif side == Cube.CUBE_LEFT:
+        elif side == CUBE_LEFT:
             y = -half_width
             x, z = meshgrid(arange(-half_width, half_width, 1),
                             arange(half_width, -half_width, -1))
-        elif side == Cube.CUBE_RIGHT:
+        elif side == CUBE_RIGHT:
             y = half_width
             x, z = meshgrid(arange(half_width, -half_width, -1),
                             arange(half_width, -half_width, -1))
-        elif side == Cube.CUBE_UP:
+        elif side == CUBE_UP:
             z = half_width
             y, x = meshgrid(arange(-half_width, half_width, 1),
                             arange(-half_width, half_width, 1))
-        elif side == Cube.CUBE_DOWN:
+        elif side == CUBE_DOWN:
             z = -half_width
             y, x = meshgrid(arange(-half_width, half_width, 1),
                             arange(half_width, -half_width, -1))
