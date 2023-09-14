ff4555e86cffad32658a717220e6f7645c386bda
lbam <lbam>
2023-09-14 05:41:08 +0000

    Add metadata processing notebook

---
+++ notebooks/process_metadata.py
@@ -0,0 +1,42 @@
+# Databricks notebook source
+import metadata
+
+# COMMAND ----------
+
+panos = metadata.read_panos("dbfs:/FileStore/tables/panorama/*/*/*/*/panorama1.csv")
+panos.write.saveAsTable("dpbk_dev.panorama.bronze_panoramas")
+miss = metadata.read_missiegegevens("dbfs:/FileStore/tables/panorama/*/missiegegevens.csv")
+miss.write.saveAsTable("dpbk_dev.panorama.bronze_missions")
+
+# COMMAND ----------
+
+panos = spark.read.table("dpbk_dev.panorama.bronze_panoramas")
+miss = spark.read.table("dpbk_dev.panorama.bronze_missions")
+
+# COMMAND ----------
+
+panos = metadata.make_api_table_panoramas(panos, miss)
+panos.write.saveAsTable("dpbk_dev.panorama.silver_panoramas", partitionBy="mission_year")
+
+# COMMAND ----------
+
+panos = spark.read.table("dpbk_dev.panorama.silver_panoramas")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC optimize dpbk_dev.panorama.silver_panoramas
+
+# COMMAND ----------
+
+miss = metadata.make_api_table_missions(miss)
+miss.write.saveAsTable("dpbk_dev.panorama.silver_missions", partitionBy="mission_year")
+
+# COMMAND ----------
+
+# Save panoramas table as a single CSV for exporting to the API.
+
+import gzip
+
+with gzip.open("/dbfs/FileStore/tables/panorama/panos_for_api.csv.gz", "wb") as f:
+    panos.toPandas().to_csv(f)
