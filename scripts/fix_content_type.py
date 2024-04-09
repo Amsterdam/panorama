8b7d9bd187b03a780dd4ea9fbbe1f48e0f1f05ee
LB <78799392+lbam@users.noreply.github.com>
2024-04-09 17:33:10 +0200

    Async fix_content_type
    About 4x faster than the sequential version.

--- scripts/fix_content_type.py
+++ scripts/fix_content_type.py
@@ -3,11 +3,13 @@
 #
 # XXX This is slow. For large collections, run multiple instances concurrently.
 
+import asyncio
 from datetime import datetime
 import os
 import sys
 
-from azure.storage.blob import BlobServiceClient, ContentSettings
+from azure.storage.blob import ContentSettings
+from azure.storage.blob.aio import BlobServiceClient
 
 
 try:
@@ -21,18 +23,33 @@ if connstr is None:
     print(f"{sys.argv[0]}: AZURE_CONNECTION_STRING not set", file=sys.stderr)
     sys.exit(3)
 
-bsc = bsc = BlobServiceClient.from_connection_string(connstr)
-cc = bsc.get_container_client("panorama")
+
 cs = ContentSettings(content_type="image/jpeg")
 
-changed = 0
-for i, b in enumerate(cc.list_blobs(name_starts_with=prefix)):
-    n = b["name"]
-    if i % 1000 == 0:
-        print(i, changed, datetime.now(), n)
-    if not n.endswith(".jpg") or b["content_settings"]["content_type"] == "image/jpeg":
-        continue
 
-    bc = cc.get_blob_client(n)
-    bc.set_http_headers(ContentSettings(content_type="image/jpeg"))
-    changed += 1
+async def main():
+    async with BlobServiceClient.from_connection_string(connstr) as bsc:
+        cc = bsc.get_container_client("panorama")
+
+        changed, i = 0, 0
+        async for page in cc.list_blobs(name_starts_with=prefix).by_page():
+            tasks = []
+            async for b in page:
+                i += 1
+                n = b["name"]
+                if (
+                    not n.endswith(".jpg")
+                    or b["content_settings"]["content_type"] == "image/jpeg"
+                ):
+                    continue
+
+                bc = cc.get_blob_client(n)
+                tasks.append(bc.set_http_headers(cs))
+
+            await asyncio.gather(*tasks)
+            changed += len(tasks)
+            print(prefix, i, changed, datetime.now())
+            continue
+
+
+asyncio.run(main())
